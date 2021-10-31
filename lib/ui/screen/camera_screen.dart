31d2fa0d2ebffd57de1e5c852e24765338209a3b
kldaw <kld.awad1996@gmail.com>
2021-10-31 07:31:24 +0200

    new commit

--- lib/ui/screen/camera_screen.dart
+++ lib/ui/screen/camera_screen.dart
@@ -7,7 +7,7 @@ class CameraScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.teal,
+        color: Colors.yellow,
       ),
     );
   }
